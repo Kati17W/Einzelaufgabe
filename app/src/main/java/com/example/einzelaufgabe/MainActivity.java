9d54e516eedfd6a5f2d8f147cec6edbab18fd54f
Katrin Weissacher <kaweissacher@edu.aau.at>
2022-03-09 12:05:16 +0100

    Design; Verknüpfung Objekte u. Views

--- app/src/main/java/com/example/einzelaufgabe/MainActivity.java
+++ app/src/main/java/com/example/einzelaufgabe/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.einzelaufgabe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button button;
+    EditText editTextNumber2;
+    TextView textView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        button = (Button) findViewById(R.id.button);
+        editTextNumber2 = (EditText) findViewById(R.id.editTextNumber2);
+        textView2 = (TextView) findViewById(R.id.textView2);
+
     }
 }
